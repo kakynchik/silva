c980c7f54efb3e8560219d36684f47e5ed5318b2
kakynchik <ukakyn@gmail.com>
2023-06-01 20:05:15 +0300

    #5

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-we use the plane to see the rainn
+def palindrome(word):
+    word = word.lower()
+    reversed_word = word[::-1]
+    return word == reversed_word
+input_word = input('vvedit slovo pzh: ')
+if palindrome(input_word):
+    print('palindrom')
+else:
+    print('ne palindrom')
