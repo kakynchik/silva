6c64bc8fdb42be06247950ac81fcf415c055f344
kakynchik <ukakyn@gmail.com>
2023-06-01 20:32:12 +0300

    #8

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-we use the plane to see the rainn
+def filter_capitalized_strings(strings):
+    filtered_strings = []
+    for string in strings:
+        if string[0].isupper():
+            filtered_strings.append(string)
+    return filtered_strings
+input_strings = input("vvedit ryadki(z probelami): ").split()
+filtered_list = filter_capitalized_strings(input_strings)
+print("ryadki s bolshoy bykvi:", filtered_list)
