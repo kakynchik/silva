2b0f7517b6bbca666ab5f845a886f92ec0353967
kakynchik <ukakyn@gmail.com>
2023-06-01 20:34:06 +0300

    #8

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-we use the plane to see the rainn
+def filter_python_strings(strings):
+    filtered_strings = []
+    for string in strings:
+        if "Python" in string:
+            filtered_strings.append(string)
+    return filtered_strings
+input_strings = input("vvedit ryadki(z probelami): ").split()
+filtered_list = filter_python_strings(input_strings)
+print("spiski ryadkov s 'Python':", filtered_list)
