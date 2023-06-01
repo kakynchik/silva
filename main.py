d5629ad37560ac28184c689833b78120743bc55d
kakynchik <ukakyn@gmail.com>
2023-06-01 20:45:35 +0300

    #12

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-we use the plane to see the rainn
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    def get_info(self):
+        print(f"name: {self.title}")
+        print(f"avtor: {self.author}")
+book1 = Book("hari poter", "dzhoan royling")
+book1.get_info()
+book2 = Book("hobbit", "Джон Рональд Руэл Толкин")
+book2.get_info()
