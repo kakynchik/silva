2529743d3c4693ae1ffd812f813b38211e4f341c
kakynchik <ukakyn@gmail.com>
2023-06-01 20:47:42 +0300

    #13

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-we use the plane to see the rainn
+class Animal:
+    def __init__(self, name, sound):
+        self.name = name
+        self.sound = sound
+    def make_sound(self):
+        print(f"{self.name} zvy4it: {self.sound}")
+animal1 = Animal("cow", "moo")
+animal1.make_sound()
+animal2 = Animal("fish", "bulk")
+animal2.make_sound()
