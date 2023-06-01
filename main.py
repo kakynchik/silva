3ed2bf9484b40f6481b7bfbe059648f89ca6d25b
kakynchik <ukakyn@gmail.com>
2023-06-01 20:37:50 +0300

    #10

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-we use the plane to see the rainn
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def get_info(self):
+        print(f"imya: {self.name}")
+        print(f"vik: {self.age}")
+student1 = Student("marchik", 16)
+student1.get_info()
+student2 = Student("zheniya", 16)
+student2.get_info()
