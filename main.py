8eeb9c149df52630709f10ece14db845863679c3
kakynchik <ukakyn@gmail.com>
2023-06-01 20:26:19 +0300

    #7

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-we use the plane to see the rainn
+students_grades = {
+    'panialbina': 90,
+    'dima': 85,
+    'marchik': 95,
+    'alek': 92,
+    'evgesha': 88
+}
+best_student = max(students_grades, key=students_grades.get)
+print("people z naybilshoyu ocinkoyu:", best_student)
