e4400a7d9375a5aa6277c2231b867768e1b36285
kakynchik <ukakyn@gmail.com>
2023-06-01 20:22:11 +0300

    #6

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-we use the plane to see the rainn
+a = int(input("vvedit pershe chislo diapazony"))
+b = int(input("vvedit druge chislo diapazony"))
+for i in range (a, b+1):
+    if 1 % 2 == 0:
+        print(i, end="")
