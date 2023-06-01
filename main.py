501193b6fbe7eabfaaccd54e5da48040d1e9bbf8
kakynchik <ukakyn@gmail.com>
2023-06-01 19:53:16 +0300

    #2

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-we use the plane to see the rainn
+start = int(input("pochatkove chislo: "))
+end = int(input("kinceve chislo: "))
+sum_of_range = 0
+for num in range(start + 1, end):
+    sum_of_range += num
+print("suma chisel y diapazoni mezhdu", start, "i", end, "=", sum_of_range)
